64205a920317f72acdc45ae9e8b22f33e53d83df
Jay-iyer <choodamj@hu-berlin.de>
2017-08-13 16:49:50 +0200

    Update SPL_Data_Cleaning_and_Feature_Engineering.R

--- SPL_Data_Cleaning_and_Feature_Engineering/SPL_Data_Cleaning_and_Feature_Engineering.R
+++ SPL_Data_Cleaning_and_Feature_Engineering/SPL_Data_Cleaning_and_Feature_Engineering.R
@@ -1,15 +1,29 @@
+#################################################################### 
+
+          ### Data Cleaning and Feature Engineering ###
+
+#################################################################### 
+
 # Load all required packages
-source("Helper_Functions.R")
+source("SPL_Helper_Functions_01.R")
+
 list.of.packages = c("rpart", "lubridate", "outliers", "rpart.plot", "xgboost", "caret", "caretEnsemble", "randomForest", 
     "e1071", "pROC", "tidyr", "klaR", "car", "devtools", "yamldebugger", "mlbench", "Hmisc", "ggvis", "relaimpo", 
     "formatR", "data.table", "zoo", "ggplot2", "forecast", "reshape2", "pdp")
+
 sapply(list.of.packages, load.packages)
 
-# Reading in original datasets
-store = read.csv("store.csv", header = T, sep = ",")  #the dataset containing supplemental information about the stores
-train = read.csv("train.csv", header = T, sep = ",")  #historical data including sales
+# Loading the datasets
+data.list = load.multiple.files(getwd(), "*csv*")
+
+store = data.list$store.csv  #the dataset containing supplemental information about 
+# the stores
+train = data.list$train.csv  #historical data including sales
+
 # Merging the datasets
 train = merge(store, train, by = "Store")
+
+### Cleaning
 
 # Replacing a typo in CompetitionOpenSinceYear variable with a median
 train$CompetitionOpenSinceYear = gsub("1900", "2010", train$CompetitionOpenSinceYear)
@@ -18,88 +32,52 @@ train$CompetitionOpenSinceYear = as.numeric(train$CompetitionOpenSinceYear)
 # Replacing empty category in PromoInterval with 'missing'
 train$PromoInterval = sub("^$", "missing", train$PromoInterval)
 
-# Turning numeric variables into factor variables
-varlist = c("StoreType", "Assortment", "Promo2", "DayOfWeek", "Open", "Promo", "StateHoliday", 
-    "SchoolHoliday", "PromoInterval")
-train[, varlist] = lapply(train[, varlist], factor)
-# Formatting the date variable
-train$Date <- as.Date(train$Date)
-
 # Removing outliers
-train <- subset(train, !(Open == 1 & Customers < 1 & Sales < 1))
+train = subset(train, !(Open == 1 & Customers < 1 & Sales < 1))
+
+### Feature engineering
 
 # Creating a starting date for Promo2
-train$Promo2SinceDate = as.Date(with(train, paste(Promo2SinceYear, Promo2SinceWeek, 
-    1, sep = "-")), "%Y-%U-%u")
+train$Promo2SinceDate = as.Date(with(train, paste(Promo2SinceYear, Promo2SinceWeek, 1, sep = "-")), "%Y-%U-%u")
 
-# Creating a starting date for Competition (assuming it's from the first day of the
-# month)
+# Creating a starting date for Competition (assuming it's from the first day of the month)
 train$CompetitionSinceDate = as.Date(paste(train$CompetitionOpenSinceYear, train$CompetitionOpenSinceMonth, 
     1, sep = "-"), format = "%Y-%m-%d")
 
 # Replacing NAs in CompetitionSinceDate
-CompetitionDateMissing = train[is.na(train$CompetitionSinceDate), ]
-CompetitionDateMissing = CompetitionDateMissing[!is.na(CompetitionDateMissing$CompetitionDistance), 
-    ]
-CompetitionDateMissing$CompetitionSinceDate = CompetitionDateMissing$Date  # replacing NAs with the current date
-train[is.na(train$CompetitionSinceDate) & !is.na(train$CompetitionDistance), "CompetitionSinceDate"] = CompetitionDateMissing$CompetitionSinceDate
-train$CompetitionSinceDate = as.Date(train$CompetitionSinceDate)
+CompetitionDateMissing = is.na(train$CompetitionSinceDate) == T & !is.na(train$CompetitionDistance) == T
+train$CompetitionSinceDate[CompetitionDateMissing == T] = train$Date[CompetitionDateMissing == T]
 
 # Create a new binary variable StoreAssortmentMatch
 train$Assortment = as.character(train$Assortment)  # StoreType and Assortment have different levels
 train$StoreAssortmentMatch = ifelse(train$StoreType == train$Assortment, 1, 0)
 
 # Create a dummy variable for whether competition is present on a given day
-train$CompetitionPresent = ifelse(train$Date < train$CompetitionSinceDate, 0, 1)
-train$CompetitionPresent = as.factor(train$CompetitionPresent)
-train$CompetitionPresent = factor(train$CompetitionPresent, levels = c(levels(train$CompetitionPresent), 
-    "missing"))
-form_na = is.na(train$CompetitionPresent)
-train[form_na, "CompetitionPresent"] = "missing"
+train$CompetitionPresent = ifelse(as.Date(train$Date) < train$CompetitionSinceDate, 0, 1)
+train$CompetitionPresent = factor(train$CompetitionPresent, levels = c("0", "1", "missing"))
+train[is.na(train$CompetitionPresent), "CompetitionPresent"] = "missing"
 
-# Create a new binary variable indicating if both types of holidays take place on a
-# given day
+# Create a new binary variable indicating if both types of holidays take place on a given day
 train$StateHoliday = as.character(train$StateHoliday)
 train$BothHolidays = ifelse(train$StateHoliday == train$SchoolHoliday, 1, 0)
 
-names <- c("Assortment", "StoreAssortmentMatch", "StateHoliday", "BothHolidays")
-train[, names] <- lapply(train[, names], factor)
+# Replace NAs in newly created CompetitionDistance, CompetitionSinceDate, Promo2SinceDate
+vars = c("CompetitionDistance", "CompetitionSinceDate", "Promo2SinceDate")
 
-# Replace NAs in newly created CompetitionDistance, CompetitionSinceDate,
-# Promo2SinceDate
-train$CompetitionDistance[is.na(train$CompetitionDistance)] = mean(train$CompetitionDistance, 
-    na.rm = TRUE)
-train$CompetitionSinceDate[is.na(train$CompetitionSinceDate)] = mean(train$CompetitionSinceDate, 
-    na.rm = TRUE)
-train$Promo2SinceDate[is.na(train$Promo2SinceDate)] = mean(train$Promo2SinceDate, na.rm = TRUE)
+for (i in 1:length(vars)) {
+    train[is.na(train[, vars[i]]), vars[i]] = mean(train[, vars[i]], na.rm = T)
+}
 
-unused.vars <- c("Promo2SinceWeek", "Promo2SinceYear", "CompetitionOpenSinceYear", "CompetitionOpenSinceMonth")
+# Fixing the datatypes of the logical variables
+factor.vars = c("StoreType", "Assortment", "Promo2", "DayOfWeek", "Open", "Promo", "StateHoliday", "SchoolHoliday", 
+    "PromoInterval", "StoreAssortmentMatch", "BothHolidays")
+train[, factor.vars] = lapply(train[, factor.vars], factor)
+
+# Removing unused variables
+unused.vars = c("Promo2SinceWeek", "Promo2SinceYear", "CompetitionOpenSinceYear", "CompetitionOpenSinceMonth")
 train[, unused.vars] = NULL
+
+
 
 # Save a full dataset for data exploration use
 saveRDS(train, "FullSet")
-
-# Creating separate test set where sales need to be predicted
-testdate = train$Date >= "2015-07-20"  # Selecting the last 10 days for forecasting
-test = train[testdate == T, ]
-train = train[testdate == F, ]  # remove test observations
-
-# Creating train dataset
-train$NewDate <- train$Date
-train$NewDate = as.factor(train$NewDate)
-set.seed(123)
-idx = createDataPartition(train$NewDate, p = 0.05, list = F)
-train_set = train[idx, ]
-train_set$NewDate = NULL
-test$NewDate = NULL
-
-# Remove the variable Sales that needs to be predicted from the testing dataset
-test$Sales = NULL
-# Remove the variable customers related to Sales from both traiing and testing
-# datasets
-test$Customers = NULL
-train_set$Customers = NULL
-
-# Saving datasets
-saveRDS(train_set, "train")
-saveRDS(test, "test")
