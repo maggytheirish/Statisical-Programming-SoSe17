c6eec9cdfcff08b36a45bdc9eb794b33302a20a4
Jay-iyer <jaya1992@gmail.com>
2017-08-17 23:05:39 +0200

    Final

--- SPL_Ensemble_06/SPL_Ensemble_06.R
+++ SPL_Ensemble_06/SPL_Ensemble_06.R
@@ -1,8 +1,11 @@
 #################################################################### 
 
-                    ### Model Evaluation ###
+                    ### Ensemble ###
 
-#################################################################### 
+####################################################################
+# The following code trains an xgboost model on the saved predictions from the 
+# previous models to pick out the best possible predictions 
+
 # Set the working directory 
 if (!require("rstudioapi")) install.packages("rstudioapi"); library("rstudioapi")
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -32,13 +35,14 @@ results = function(model,modelname,data, actual) {
       # Error decomposition
       mse = mean((actual - pred)^2)
       bias = (mean(pred) - mean(actual))
-      diff.var = var(actual) - var(pred)  
+      var = mse - (bias^2) 
+      percent.var = var/mse
       actual.skew = skewness(actual)  # to check the distributions
       predicted.skew = skewness(pred)
       
       # Saving results
       
-      error.matrix = cbind(error, mse, bias, diff.var, actual.skew, predicted.skew)
+      error.matrix = cbind(error, mse, bias, var, percent.var ,actual.skew, predicted.skew)
       cat("\n", "Decomposing the error : ", "\n")
       print(error.matrix)
       
@@ -69,17 +73,18 @@ predictions = readRDS("Predictions_test.RDS")
 ## Split into two DFs, 50% each, based on Sales
 idx_ensemble = createDataPartition(y = predictions$actual, p = 0.5, list = FALSE)
 
-trainset_ensemble = predictions[idx_ensemble,]
-testset_ensemble = predictions[-idx_ensemble,]
+trainset_ensemble = predictions[idx_ensemble,] #using one half of the predictions for training
+testset_ensemble = predictions[-idx_ensemble,] #independent testset
 
 #Remove actual values
-testset_ensemble$actual = NULL
+sales_testset = testset_ensemble$actual # saving actual values for comparison
+testset_ensemble$actual = NULL #Removing actual values from the test set
 
 # Train Ensemble
 
 # XGB Model Setup
 model.control= trainControl(method = "cv",number = 5,allowParallel = TRUE)
-
+# Setting parameters
 xgb.parms.default = expand.grid(nrounds = 400, 
                                  max_depth = 10, 
                                  eta = 0.05, 
@@ -96,4 +101,16 @@ xgb.ensemble = caret::train(actual~., data = trainset_ensemble,
                              trControl = model.control)
 
 # Predict 
-xgb.ensemble.pred = results(xgb.ensemble,"xgb.ensemble",testset_ensemble, predictions$actual)
+xgb.ensemble.pred = results(xgb.ensemble,"xgb.ensemble",testset_ensemble,sales_testset)
+
+# Comparing rmse values for the different models on the testset
+for(i in c(1:5)){
+  error = matrix(NA,nrow=1,ncol = 5)
+  colnames(error) = colnames(testset_ensemble)
+  for(i in c(1:5)){
+  error[1,i] = rmse(sales_testset,as.vector(testset_ensemble[,i]))}
+  error = as.data.frame(error)
+}
+
+error$ensemble = rmse(sales_testset,xgb.ensemble.pred) #adding rmse from ensemble 
+print(error)
